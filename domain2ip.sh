7553006910d61ffe6655508e1564770ae367e9f1
Sengshin Lee <sengshinlee@gmail.com>
2022-10-21 14:39:51 +0800

    Update domain2ip.sh

--- domain2ip.sh
+++ domain2ip.sh
@@ -8,8 +8,8 @@ function domain2ip() {
 }
 
 function download_hosts_go_latest_apk() {
-    HOSTS_GO_URL="http://d.apkpure.com/b/APK/dns.hosts.server.change?version=latest"
-    wget -t 0 ${HOSTS_GO_URL} -O hosts-go-latest.apk
+    HOSTS_GO_URL="https://d.apkpure.com/b/APK/dns.hosts.server.change?version=latest"
+    wget -t 0 -U NoSuchBrowser/1.0 ${HOSTS_GO_URL} -O hosts-go-latest.apk
 }
 
 function main() {
